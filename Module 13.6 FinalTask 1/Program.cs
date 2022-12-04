9e57e7fcebd8affd6bd073984906f3d6b6d6b114
Nikula Daniil <daniil.nikula@gmail.com>
2022-12-04 13:52:39 +0200

    Добавьте файлы проекта.

---
+++ Module 13.6 FinalTask 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Module_13._6_FinalTask_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            var strings = new List<string>();
+            string text = File.ReadAllText("D:\\Domeny\\Downloads\\Text1.txt");
+            strings.Add(text);
+            Console.WriteLine($"List Time Elapsed: {stopWatch.Elapsed.TotalMilliseconds} ms");//8ms
+
+            stopWatch = Stopwatch.StartNew();
+            LinkedList<string> LinkedList = new LinkedList<string>();
+            text = File.ReadAllText("D:\\Domeny\\Downloads\\Text1.txt");
+            LinkedList.AddFirst(text);
+            Console.WriteLine($"LinkedList Time Elapsed: {stopWatch.Elapsed.TotalMilliseconds} ms");//4ms, Two times faster.
+        }
+    }
+}
